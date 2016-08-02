6e36f6c41c9530ab3b113be6b277f935c92d6bba
underdolphin <underdolphin@users.noreply.github.com>
2016-08-03 01:27:32 +0900

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,68 @@
 /// <reference path="typings/index.d.ts" />
-
+'use strict'
+const child_process = require('child_process');
+const spawn = child_process.spawn;
 const gulp = require('gulp');
+const nodemon = require('gulp-nodemon');
 const gulpts = require('gulp-typescript');
 const prettydiff = require('gulp-prettydiff');
+const path = require('path');
 
 const serverProject = gulpts.createProject('tsconfig.json', {
     typescript: require('typescript'),
-    module : "commonjs"
+    module: "commonjs"
 });
 
 const tsProject = gulpts.createProject('tsconfig.json', {
     typescript: require('typescript'),
-    module : "es2015"
+    module: "es2015"
 });
 
 gulp.task('serverCompile', () => {
-    gulp.src('src/**/*.ts')
+    gulp.src('src/server/**/*.ts')
         .pipe(gulpts(serverProject))
         .js
         .pipe(prettydiff({
             lang: 'js',
             mode: 'minify'
         }))
-        .pipe(gulp.dest('build/'));
+        .pipe(gulp.dest('build/server'));
 });
 
-gulp.task('serverCompile', () => {
-    gulp.src('src/**/*.ts')
+gulp.task('tsCompile', () => {
+    gulp.src('src/client/**/*.ts')
         .pipe(gulpts(tsProject))
         .js
         .pipe(prettydiff({
             lang: 'js',
             mode: 'minify'
         }))
-        .pipe(gulp.dest('build/'));
+        .pipe(gulp.dest('build/client'));
 });
 
 gulp.task('nodemon', (cb) => {
-    let called = false;
-
-    return nodemon({
-        script: 'src/server/js/',
-        watch: 'src/server/',
-        ext: 'ts',
-        tasks: (changedFiles) => {
-            let tasks = [];
-            changedFiles.forEach(file => {
-                if (path.extname(file) === '.ts' && !~tasks.indexOf("serverCompile"))
-                    tasks.push("serverCompile");
-            });
-            return tasks;
-        }
-    });
+    nodemon({
+        script: 'build/server/',
+        watch: 'build/',
+        ignore: ['build/client/**/*']
+    }).on('start', cb);
 });
 
-gulp.task('default', ['tsCompile'], () => {
-    gulp.watch('src/**/*.ts', ['tsCompile']);
+gulp.task('watch', ['tsCompile', 'serverCompile','start'], () => {
+    gulp.watch('src/server/**/*.ts', ['serverCompile']);
+    gulp.watch('src/client/**/*.ts', ['tsCompile']);
+});
+
+gulp.task('start', () => {
+    gulp.start('nodemon');
+});
+
+gulp.task('default', () => {
+    let process;
+    function restart() {
+        if (process) process.kill();
+        process = spawn('gulp', ['watch'], { stdio: 'inherit' });
+    }
+    gulp.watch('gulpfile.js', restart);
+    restart();
 });
